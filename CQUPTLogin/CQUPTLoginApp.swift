55e34bb81cb29e0a57b8bd3865db67fc3ded43b1
jerrita <je5r1ta@icloud.com>
2022-11-02 16:51:54 +0800

    Initial Commit

---
+++ CQUPTLogin/CQUPTLoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  CQUPTLoginApp.swift
+//  CQUPTLogin
+//
+//  Created by Jerrita on 2022/11/2.
+//
+
+import SwiftUI
+
+@main
+struct CQUPTLoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
